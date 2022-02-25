25bdd571acb2ea746a7754d92c06f745704878b9
Yashvi1620 <yashvipatwa16@gmail.com>
2022-02-25 12:04:12 +0530

    role

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const mongoose = require("mongoose")
 
 const sessionController = require("./controller/session-controller")
 const roleController = require("./controller/role-controller")
-
-
+const userController=require("./controller/user-controller")
+const projectController=require("./controller/project-controller")
+const project_teamcontroller=require("./controller/project_teamcontroller")
+const status_controller=require("./controller/status-controller")
+//const project_module_controller=require("./controller/project-module-contr
+const project_module_controller=require("./controller/project-module-controller")
+const task_controller=require("./controller/task-controller")
+const user_taskcontroller=require("./controller//user-task-controller")
+const prioritycontroller=require("./controller/priority-controller")
 
 const app = express()
 //middle ware 
@@ -40,6 +47,49 @@ app.post("/roles",roleController.addRole)
 app.get("/roles",roleController.getAllRoles)
 app.delete("/roles/:roleId",roleController.deleteRole)
 app.put("/roles",roleController.updateRole)
+
+//user
+app.post("/users",userController.addUser)
+app.post("/users",userController.getAllUsers)
+app.delete("/users/:userId",userController.deleteUser)
+app.put("/users",userController.updateUser)
+app.post("/login",userController.login)
+
+//project
+app.post("/project",projectController.addProject)
+app.get("/project",projectController.getAllProject)
+app.delete("/project/:projectId",projectController.deleteProject)
+app.put("/project",projectController.updateProject)
+//project_team
+app.post("/project_team",project_teamcontroller.addteam)
+app.get("/project_team",project_teamcontroller.getAllProjectTeam)
+app.delete("/project_team/:project_teamId",project_teamcontroller.deleteProjectTeam)
+//status
+app.post("/status",status_controller.addstatus)
+app.get("/status",status_controller.getAllstatus)
+app.delete("/status/:statusId",status_controller.deletestatus)
+app.put("/status",status_controller.updatestatus)
+//module
+app.post("/module",project_module_controller.addmodule)
+app.get("/module",project_module_controller.getAllmodule)
+
+app.delete("/module/:moduleid",project_module_controller.deletemodule)
+app.put("/module",project_module_controller.updatemodule)
+//task
+app.post("/task",task_controller.addtask)
+app.get("/task",task_controller.getAlltask)
+app.delete("/task",task_controller.deletetask)
+app.put("/task",task_controller.updatetask)
+//priority
+app.post("/priority",prioritycontroller.addPriority)
+app.get("/priority",prioritycontroller.getAllPriority)
+app.delete("/priority",prioritycontroller.deletePriority)
+app.put("/priority",prioritycontroller.updatePriority)
+//user_taskcontroller
+app.post("/user_taskcontroller",user_taskcontroller.addUserTask)
+app.get("user_taskcontroller",user_taskcontroller.getAllUserTask)
+app.delete("user_taskcontroller",user_taskcontroller.deleteUserTask)
+app.put("user_taskcontroller",user_taskcontroller.updateUserTask)
 
 
 //server 
