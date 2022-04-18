0520a541deec1487ee5d264bc2b7386201a40752
Yashvi1620 <96307761+Yashvi1620@users.noreply.github.com>
2022-04-18 11:41:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors=require("cors")
 
 const sessionController = require("./controller/session-controller")
 const roleController = require("./controller/role-controller")
@@ -7,13 +8,14 @@ const userController=require("./controller/user-controller")
 const projectController=require("./controller/project-controller")
 const project_teamcontroller=require("./controller/project_teamcontroller")
 const status_controller=require("./controller/status-controller")
-//const project_module_controller=require("./controller/project-module-contr
+
 const project_module_controller=require("./controller/project-module-controller")
 const task_controller=require("./controller/task-controller")
 const user_taskcontroller=require("./controller//user-task-controller")
 const prioritycontroller=require("./controller/priority-controller")
 
 const app = express()
+app.use(cors())
 //middle ware 
 app.use(express.json()) //mobile -> accept json data from request and set data into body 
 app.use(express.urlencoded({extended:true})) //web --> accept url encoded data from request and set data into body  
@@ -50,16 +52,19 @@ app.put("/roles",roleController.updateRole)
 
 //user
 app.post("/users",userController.addUser)
-app.post("/users",userController.getAllUsers)
+app.get("/users",userController.getAllUsers)
 app.delete("/users/:userId",userController.deleteUser)
 app.put("/users",userController.updateUser)
 app.post("/login",userController.login)
+
 
 //project
 app.post("/project",projectController.addProject)
 app.get("/project",projectController.getAllProject)
 app.delete("/project/:projectId",projectController.deleteProject)
 app.put("/project",projectController.updateProject)
+app.get("/project/:projectId",projectController.getoneProject)
+
 //project_team
 app.post("/project_team",project_teamcontroller.addteam)
 app.get("/project_team",project_teamcontroller.getAllProjectTeam)
@@ -70,30 +75,33 @@ app.get("/status",status_controller.getAllstatus)
 app.delete("/status/:statusId",status_controller.deletestatus)
 app.put("/status",status_controller.updatestatus)
 //module
-app.post("/module",project_module_controller.addmodule)
-app.get("/module",project_module_controller.getAllmodule)
-
-app.delete("/module/:moduleid",project_module_controller.deletemodule)
-app.put("/module",project_module_controller.updatemodule)
+app.post("/project_module",project_module_controller.addmodule)
+//app.get("/project_module/:prjectId",project_module_controller.getOneemodule)
+app.get("/project_module",project_module_controller.getAllmodule)
+app.get("/project_module/:projectId",project_module_controller.getoneModule)
+app.delete("/project_module/:moduleid",project_module_controller.deletemodule)
+app.put("/project_module",project_module_controller.updatemodule)
 //task
 app.post("/task",task_controller.addtask)
 app.get("/task",task_controller.getAlltask)
-app.delete("/task",task_controller.deletetask)
+app.delete("/task/:taskid",task_controller.deletetask)
+app.get("/updatetask/:taskid",task_controller.getoneTask)
 app.put("/task",task_controller.updatetask)
+app.get("/task/:moduleId",task_controller.getOneTaskbymodule)
 //priority
 app.post("/priority",prioritycontroller.addPriority)
 app.get("/priority",prioritycontroller.getAllPriority)
 app.delete("/priority",prioritycontroller.deletePriority)
 app.put("/priority",prioritycontroller.updatePriority)
 //user_taskcontroller
-app.post("/user_taskcontroller",user_taskcontroller.addUserTask)
-app.get("user_taskcontroller",user_taskcontroller.getAllUserTask)
-app.delete("user_taskcontroller",user_taskcontroller.deleteUserTask)
-app.put("user_taskcontroller",user_taskcontroller.updateUserTask)
+app.post("/user_task",user_taskcontroller.addUserTask)
+app.get("/user_task",user_taskcontroller.getAllUserTask)
+app.delete("/user_task/:UserTaskId",user_taskcontroller.deleteUserTask)
+app.put("/user_task",user_taskcontroller.updateUserTask)
 
 
 //server 
 
-app.listen(3000,function(){
-  console.log("server started on 3000");  
+app.listen(4000,function(){
+  console.log("server started on 4000");  
 })
